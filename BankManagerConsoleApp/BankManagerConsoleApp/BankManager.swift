b2ccb1590528142fadb3d0d89a76ef6b584bf7b7
woong <workplayhard1@naver.com>
2022-11-08 17:24:01 +0900

    feat: Banker타입 내부 프로퍼티 추가 및 관련 메서드 수정, Customer타입 내부 프로퍼티 추가
    - BankManager타입의 addCustomer, addBankers 메서드 수정
    - ConsoleApp addBankers 파라미터 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
@@ -9,13 +9,14 @@ struct BankManager {
     
     mutating func addCustomers(count: Int) {
         for customerNumber in 1...count {
-            let customer: Customer = Customer(number: customerNumber)
+            guard let bankBusiness: BankBusiness = BankBusiness.allCases.randomElement() else { return }
+            let customer: Customer = Customer(number: customerNumber, bankBusiness: bankBusiness)
             bank.addCustomerToQueue(customer)
         }
     }
     
-    mutating func addBankers(count: Int) {
-        for banker in Array.init(repeating: Banker(), count: count) {
+    mutating func addBankers(bankBusiness: BankBusiness, count: Int) {
+        for banker in Array.init(repeating: Banker(bankBusiness: bankBusiness), count: count) {
             bank.addBanker(banker)
         }
     }
